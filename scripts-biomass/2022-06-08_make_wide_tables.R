0d92d07360fff5d4fd18b1b25d37599d19ac5e48
Robin Rohwer <robin.rohwer@gmail.com>
2022-06-09 08:03:11 -0700

    removed a problem row. count was zero, but biomass was outlier high. And org was only present at all 3 times total.

--- scripts-biomass/2022-06-08_make_wide_tables.R
+++ scripts-biomass/2022-06-08_make_wide_tables.R
@@ -12,11 +12,37 @@ created.file <- "robin-data/2022-06-08_phyto_list.rds"
 
 # ----
 
+# There's a mistake in the data on 2011-07-25, total biomass is 104
+# shows up in biomass, not biovolume. so likely a conversion error, no?
+
+problem.day <- which(phyto$Date == parse_date_time("2011-07-25","ymd", "Etc/GMT-5"))
+
+View(phyto[problem.day, ])
+
+problem.row <- 5423
+
+phyto[problem.row, ] 
+
+coelastrum.reticulatum <- which(phyto$lter.taxa_name == "Coelastrum reticulatum")
+
+plot(x = phyto$Date[coelastrum.reticulatum], y = phyto$Biovolume.um3.mL[coelastrum.reticulatum])
+plot(x = phyto$Date[coelastrum.reticulatum], y = phyto$Count.cells.mL[coelastrum.reticulatum])
+
+# It's not like this is a common organism. Just remove the problem row, leave the rest of the date.
+phyto <- phyto[-problem.row, ]
+
+# ----
+
 colnames(phyto)
 
 phyto.division <- pivot_wider(data = phyto, id_cols = "lter.division", names_from = "Date", values_from = "Biomass.mg.L", values_fn = sum)
 phyto.genus <- pivot_wider(data = phyto, id_cols = "lter.genus", names_from = "Date", values_from = "Biomass.mg.L", values_fn = sum)
 phyto.name <-pivot_wider(data = phyto, id_cols = "lter.taxa_name", names_from = "Date", values_from = "Biomass.mg.L", values_fn = sum)
+
+# phyto.division <- pivot_wider(data = phyto, id_cols = "lter.division", names_from = "Date", values_from = "Biovolume.um3.mL", values_fn = sum)
+# phyto.genus <- pivot_wider(data = phyto, id_cols = "lter.genus", names_from = "Date", values_from = "Biovolume.um3.mL", values_fn = sum)
+# phyto.name <-pivot_wider(data = phyto, id_cols = "lter.taxa_name", names_from = "Date", values_from = "Biovolume.um3.mL", values_fn = sum)
+
 
 # stupid tibbles
 
@@ -32,7 +58,7 @@ phyto.division <- un.tibble.it(stupid.phyto = phyto.division)
 phyto.genus <- un.tibble.it(stupid.phyto = phyto.genus)
 phyto.name <- un.tibble.it(stupid.phyto = phyto.name)
 
-phyto.total <- data.frame("date" = colnames(phyto.division), "biomass" = colSums(phyto.division))
+phyto.total <- data.frame("date" = colnames(phyto.division), "biomass" = colSums(phyto.division, na.rm = TRUE))
 phyto.total$date <- parse_date_time(x = phyto.total$date, orders = "ymd")
 
 my.phyto <- list("tot" = phyto.total, "div" = phyto.division, "gen" = phyto.genus, "tax" = phyto.name)
