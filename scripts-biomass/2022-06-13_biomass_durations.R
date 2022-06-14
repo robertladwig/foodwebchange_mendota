b82fbf84282a60ea6c41f5e9bdba5783ec807da0
Robin Rohwer <robin.rohwer@gmail.com>
2022-06-14 14:47:16 -0700

    added export of duration data for Robert

--- scripts-biomass/2022-06-13_biomass_durations.R
+++ scripts-biomass/2022-06-13_biomass_durations.R
@@ -34,7 +34,8 @@ p.val <- t.test(x = boxes[[1]], y = boxes[[2]])
 p.val$p.value
 
 # ----
-pdf(file = "plots/2022-06-13_biomass_duration_boxplot.pdf", width = 3, height = 3)
+# pdf(file = "plots/2022-06-13_biomass_duration_boxplot.pdf", width = 3, height = 3)
+pdf(file = "figs/biomass_duration_1mgL.pdf", width = 3, height = 3)
 par(mar = c(3,3.5,1,.5))
 x.locs <- boxplot(x = boxes, axes = F, col = c("steelblue","orange2"))
 # points(x = jitter(x = rep(1,length(boxes[[1]])), factor = 4), y = boxes[[1]])
@@ -47,4 +48,46 @@ axis(side = 2, las = 2, labels = T, line = -.5, lwd = 0)
 axis(side = 1, lwd = 0, lwd.ticks = 1, labels = F, at = c(1,2))
 mtext(text = "p < 0.005", side = 1, line = -1.25, at = 2.3, cex = .7)
 dev.off()
-# ----        
+
+# ---- save total high days for Robert ----
+
+x <- data.frame("Year" = as.numeric(sub(pattern = "\\.bio", replacement = "", x = colnames(phyto))),
+                "Days.0.5.mg.L" = NA, "Days.1.mg.L" = NA, "Days.1.5.mg.L" = NA, "Days.2.mg.L" = NA, "Days.3.mg.L" = NA)
+
+high.biomass <- .5
+phyto.high <- phyto > high.biomass
+total.high.days <- colSums(phyto.high, na.rm = T)
+x$Days.0.5.mg.L <- total.high.days
+
+high.biomass <- 1
+phyto.high <- phyto > high.biomass
+total.high.days <- colSums(phyto.high, na.rm = T)
+x$Days.1.mg.L <- total.high.days
+
+high.biomass <- 1.5
+phyto.high <- phyto > high.biomass
+total.high.days <- colSums(phyto.high, na.rm = T)
+x$Days.1.5.mg.L <- total.high.days
+
+high.biomass <- 2
+phyto.high <- phyto > high.biomass
+total.high.days <- colSums(phyto.high, na.rm = T)
+x$Days.2.mg.L <- total.high.days
+
+high.biomass <- 3
+phyto.high <- phyto > high.biomass
+total.high.days <- colSums(phyto.high, na.rm = T)
+x$Days.3.mg.L <- total.high.days
+
+barplot(x$Days.0.5.mg.L, names.arg = x$Year, las = 2, main = .5)
+barplot(x$Days.1.mg.L, names.arg = x$Year, las = 2, main = 1)
+barplot(x$Days.1.5.mg.L, names.arg = x$Year, las = 2, main = 1.5)
+barplot(x$Days.2.mg.L, names.arg = x$Year, las = 2, main = 2)
+barplot(x$Days.3.mg.L, names.arg = x$Year, las = 2, main = 3)
+boxplot(list("Before\n(1995-2009)" = x$Days.0.5.mg.L[1:15], "After\n(2010-2020)" = x$Days.0.5.mg.L[16:26]), main = ".5")
+boxplot(list("Before\n(1995-2009)" = x$Days.1.mg.L[1:15], "After\n(2010-2020)" = x$Days.1.mg.L[16:26]), main = "1")
+boxplot(list("Before\n(1995-2009)" = x$Days.1.5.mg.L[1:15], "After\n(2010-2020)" = x$Days.1.5.mg.L[16:26]), main = "1.5")
+boxplot(list("Before\n(1995-2009)" = x$Days.2.mg.L[1:15], "After\n(2010-2020)" = x$Days.2.mg.L[16:26]), main = "2")
+boxplot(list("Before\n(1995-2009)" = x$Days.3.mg.L[1:15], "After\n(2010-2020)" = x$Days.3.mg.L[16:26]), main = "3")
+
+write.csv(x = x, file = "output/biomass_duration.csv", row.names = F)
