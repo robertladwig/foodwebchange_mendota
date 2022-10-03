5b97bd3e052c55c34e606b90461a2054323fb081
Robin Rohwer <robin.rohwer@gmail.com>
2022-10-03 14:11:28 -0500

    updated stats with new kruskal-based p-values

--- scripts-biomass/2022-10-02_stats_for_text.R
+++ scripts-biomass/2022-10-02_stats_for_text.R
@@ -2,9 +2,9 @@
 options(scipen = 999)
 
 
-# After the spiny water flea invasion, spring average biomass increased significantly from 1 ± 1 to 3 ± 2 (p = 0.03)
-# During ice-on, average biomass also increased significantly (p = 0.02), however the total biomass is much lower during this season.
-# We found that after the spiny water flea invasion, the lag between stratification onset and anoxia onset decreased by almost 2 weeks, from 51 ± 9 days to 39 ± 15 days (p = 0.02)
+# After the spiny water flea invasion, spring average phytoplankton biomass increased 2-fold to levels previously typical of stratification, from 1 ± 1 to 3 ± 2 mg/L (p = 0.005). 
+# Similarly, ice-on biomass increased to levels previously typical of spring, from 0.3 ± 0.3 to 2 ± 2 mg/L (p = 0.003) 
+# the lag between stratification onset and anoxia onset decreased by almost 2 weeks, from 51 ± 9 days to 39 ± 15 days (p = 0.02)
 x <- read.csv(file = "plots/2022-10-02_spring_importance_plot/mean_annual_biomass_by_season-stats.csv")
 x
 
@@ -19,8 +19,8 @@ x$spring
 x$spring$post[1] / x$spring$pre[1] # 2-fold, p = .1
 
 # up with diatoms:
-# Chlorophyta (green algae) remained at 5-9 percent abundance but increased 4-fold, from 0.04 ± .02 to 0.1 ± 0.1 mg/L (p = 0.003)
-# Blue-green algae was more variable, but increased by 6 fold, from 0.03 ± 0.03 to 0.2 ±  0.2 (p = 0.06)
+# Chlorophyta (green algae) remained at 5-9 percent abundance but increased 4-fold, from 0.04 ± .02 to 0.1 ± 0.1 mg/L (p = 0.0004)
+# Blue-green algae was more variable, but increased by 6 fold, from 0.03 ± 0.03 to 0.2 ±  0.2 (p = 0.004)
 # Pyrrhophyta (dinoflagellates) remained at 1-3 percent of the community but increased by 3-fold, from 0.02 ± 0.03 to 0.05 ± 0.03 (p = 0.02)
 # down:
 # Cryptophyta (cryptophytes) decreased from 17 ± 12 to 9 ± 6 \% of the community (p = 0.04)
